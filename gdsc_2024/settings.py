4a71dddbde45c6b1c3a5a4f24280de42c19f91f8
Favour Olumese <olumesefavor@gmail.com>
2024-04-02 09:59:16 +0100

    Day 2 Task
    Now that you have your HTTP server, you will create an endpoint, /hello, this endpoint should return the Hello GDSC in bold. [Hint: Use HTML tags for this]. Another endpoint, /api/hello should return the same thing but this time in JSON form with the message property set to “Hello GDSC”.

--- gdsc_2024/settings.py
+++ gdsc_2024/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ird%x%4^!*&=wvfg3@$+x!i+jby*3v2enq4_9)w6(4!iswz-__
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gdsc_2024_project',
 ]
 
 MIDDLEWARE = [
--- gdsc_2024_project/views.py
+++ gdsc_2024_project/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def fav(request):
+    return JsonResponse({'Expectation': 'Looking forward to great things.'})
+
+def home(request):
+    return render(request, 'gdsc_2024_project/home.html')
+
+def home_api(request):
+    return JsonResponse({'message': 'Hello GDSC'})
