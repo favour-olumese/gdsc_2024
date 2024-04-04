f0404b20c1dbf2a4e7a8bdf71523c104a8f919d9
Favour Olumese <olumesefavor@gmail.com>
2024-04-04 21:49:01 +0100

    Creating a Model or Database Table
    You have your database connected now but need a table to hold certain data. You are creating an API for a bookstore. Each book has to be identified uniquely and possesses properties that help in proper categorization. Decide what the properties or fields of this Book Model will be and create a Database Table for it.

--- gdsc_2024_project/models.py
+++ gdsc_2024_project/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    book_name = models.CharField(max_length=200)
+    publisher = models.ForeignKey('Publisher', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.book_name
+    
+class author(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+class publisher(models.Model):
+    pub_name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.pub_name
